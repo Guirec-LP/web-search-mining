9f5aca00a30c01015cc41f2d9d0074484414fc7b
unknown <guirec.lepoulennec@hotmail.fr>
2018-06-11 14:17:57 +0800

    now we can retrieve the title and the body of a website

--- crawler.js
+++ crawler.js
@@ -2,8 +2,9 @@ var request = require('request')
 var cheerio = require('cheerio')
 var URL = require('url-parse')
 
-var pageToVisit = "http://www.arstechnica.com";
+var pageToVisit = "http://www.gutenberg.org/files/11/11-h/11-h.htm";
 console.log("Visiting page " + pageToVisit);
+
 request(pageToVisit, function(error, response, body) {
    if(error) {
      console.log("Error: " + error);
@@ -14,5 +15,12 @@ request(pageToVisit, function(error, response, body) {
      // Parse the document body
      var $ = cheerio.load(body);
      console.log("Page title:  " + $('title').text());
+     console.log(harvestWords($));
    }
 });
+
+
+function harvestWords($) {
+  var bodyText = $('html > body').text();
+    return bodyText;
+}
