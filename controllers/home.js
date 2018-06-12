6ab4f34419f5d7f6efcff3ab4e5b3b7ffd2c1da0
unknown <guirec.lepoulennec@hotmail.fr>
2018-06-13 06:59:38 +0800

    Async to schedule the tasks and more visibility Corrected the creation of postings to avoid empty items Save the postings frequencies into the database Display the titles of the books preisouly saved on the front page (html)

--- controllers/home.js
+++ controllers/home.js
@@ -1,19 +1,19 @@
 var template = require('../views/template-main');
-var mongo_data = require('../model/mongo-data');  
+var mongo_data = require('../model/mongo-data');
 exports.get = function(req, res) {
-  mongo_data.teamlist("D", function(err, teamlist) {
+  mongo_data.postings(function(err, savedBooks) {
     if (!err) {
-      var strTeam = "",
-        i = 0;
-      for (i = 0; i < teamlist.count;) {
-        strTeam = strTeam + "<li>" + teamlist.teams[i].country + "</li>";
-        i = i + 1;
-      }
-      strTeam = "<ul>" + strTeam + "</ul>";
+
       res.writeHead(200, {
         'Content-Type': 'text/html'
       });
-      res.write(template.build("Test web page on node.js", "Hello there", "<p>The teams in Group " + teamlist.GroupName + " for Euro 2012 are:</p>" + strTeam));
+      var title = "Test web page on node.js"
+      var pageTitle = "Web Search and Mining - Implementation"
+      var content = "<p>The postings are saved as : </p>"
+      savedBooks.forEach(function(title){
+        content += '<p>'+title+'</p>'
+      })
+      res.write(template.build(title,pageTitle,content));
       res.end();
     } else {
       res.writeHead(200, {
--- crawler.js
+++ crawler.js
@@ -1,84 +1,121 @@
 var request = require('request')
 var cheerio = require('cheerio')
-// var URL = require('url-parse')
+var mongo = require('mongodb')
+async = require('async')
+
+  Server = mongo.Server
+  Db = mongo.Db
+var server = new Server('localhost', 27017, {
+  auto_reconnect: true
+});
+var db = new Db('webSearch', server);
+var onErr = function(err, callback) {
+  db.close();
+  callback(err);
+};
+var collection
+
+db.open(function(err, db) {
+  if (!err) {
+    collection = db.collection('postings',function(err,collection){
+      if(err){
+        onErr(err, callback);
+      }
+    })
+  }else{
+      onErr(err,callback)
+  }
+})
 
 
-var maxPages = 50;
-var start = 3
-for(var i = start ; i-start<maxPages ; i++){
-  targetPage = "http://www.gutenberg.org/files/"+i+"/"+i+"-h/"+i+"-h.htm";
-  console.log("* "+(i-start+1)+" * "+"Crawling : "+targetPage)
-  crawlSinglePage(targetPage,function(result){
-    var message = "  - Number of words for the posting : "+ result.length
-    console.log(message)
-  })
-}
+  var myPostingsFrequency = ['ici']
+  var maxPages = 1;
+  var start = 3
+  var rootURL = "http://www.gutenberg.org/files/"
 
+  console.log("Crawling "+maxPages+" pages from gutenberg.org")
+
+  for(var i = 0 ; i<maxPages ; i++){
+    var index = i + start
+    targetPage = rootURL+index+"/"+index+"-h/"+index+"-h.htm";
+    var frequencyPostings = []
+    var title
+    async.waterfall([
+        function(callback) {
+          callback(null, targetPage);
+        },
+        crawlSinglePage,
+        filtering,
+        createPostings,
+        calculateFrequencies,
+    ], function (error, result) {
+        if (error) { alert('Something is wrong !');
+      }else{
+        crawlingSuccess(result,targetPage,title);
+
+      }
+    });
+  }
+
+  function crawlingSuccess(result,targetPage,title){
+    frequencyPostings = result;
+    console.log( "   -> frequency postings done ("+ Object.keys(frequencyPostings).length+' different words)')
+
+    // do something with the frequency postings
+    var document = {"url":targetPage, "title":title, "postings":frequencyPostings};
+
+    collection.save(document, {w: 1}, function(err, records){
+        if(err){
+          console.log(err);
+        }
+    });
+    collection.find().toArray(function(err, data){
+        if(err){
+          console.log(err);
+        }else{
+          data.forEach(function(element){
+            console.log(element['title'])
+          });
+        }
+    });
+  }
 
 function crawlSinglePage(targetPage, callback){
   request(targetPage, function(error, response, body) {
      if(error) {
        console.log("Error: " + error);
-     }
-     // Check status code (200 is HTTP OK)
-     console.log("  - HTML file downloaded");
-     if(response.statusCode === 200) {
-       // Parse the document body
-       var $ = cheerio.load(body);
-       var title=  $('title').text().trim();
+     }else{
+       // Check status code (200 is HTTP OK)
+       // console.log("  - HTML file downloaded");
+       if(response.statusCode === 200) {
+         // Parse the document body
+         var $ = cheerio.load(body);
+         title=  $('title').text().trim();
 
-       if(title.startsWith("The Project Gutenberg eBook of")) {
-           title = title.split('The Project Gutenberg eBook of')[1];
-       }else if (title.startsWith('The Project Gutenberg EBook of')) {
-          title = title.split('The Project Gutenberg EBook of')[1];
+         if(title.startsWith("The Project Gutenberg eBook of")) {
+             title = title.split('The Project Gutenberg eBook of')[1];
+         }else if (title.startsWith('The Project Gutenberg EBook of')) {
+            title = title.split('The Project Gutenberg EBook of')[1];
 
-       }else if(title.startsWith('The Project Gutenberg\'s etext of')){
-          title = title.split('The Project Gutenberg\'s etext of')[1];
+         }else if(title.startsWith('The Project Gutenberg\'s etext of')){
+            title = title.split('The Project Gutenberg\'s etext of')[1];
+         }
+         console.log(" *  "+title.substring(0,65)+' [...]')
+         var body = $('html > body').text();
+
+         // here we identifu the pre div to remove it from the useful text
+         var pre= $('pre').text()
+         body = body.split(pre).join(' ')
+
+         var frequencyPostings = [] ;
+         // Here we define the correct order to avoid a callbeck hell situation
+         callback(null,body)
        }
-       console.log("  - Title (short) : "+title.substring(0,35)+'...')
-       var frequency
-       bookParse($,function(result){
-         frequency = result
-         console.log('  - Successful parsing of the book')
-       });
-       callback(frequency)
      }
   });
 }
 
-function bookParse($,callback) {
-    var body = $('html > body').text();
 
-    // here we identifu the pre div to remove it from the useful text
-    var pre= $('pre').text()
-    body = body.split(pre).join(' ')
-
-
-    var filteredWords
-    filtering(body,function(result){
-      filteredWords = result
-      console.log('  - Successful filtering of the book')
-    })
-    var totalNbWords = filteredWords.length
-
-    // Creation of the postings
-    var postings = [] ;
-    createPostings(filteredWords,function(result){
-      postings = result;
-      console.log('  - Successful creation of the postings')
-    })
-
-    sortedPostings = sortTableByValue(postings);
-    // displaySimpleTable(filteredWords)
-    // displayAssociativeTable(sortedPostings)
-    var frequencyPostings = []
-    calculateFrequencies(sortedPostings,totalNbWords,function(result){
-      frequencyPostings = result;
-      console.log('  - Successful creation of the frequency postings')
-    })
-
-    callback(frequencyPostings);
-}
 
 function filtering(body,callback){
     // removes the parts that belongs to Gutenberg's Project
@@ -86,7 +123,7 @@ function filtering(body,callback){
     // cut all the text into uniqu
     var tmp = body.replace(/(\r\n|\n|\r|\n\r)/gm," ");
     var tmp = tmp.replace(/^[a-z,A-Z,0-9]/gm," ");
-    var tmp = tmp.replace(/[;‘’“'"—_—\-,.!:?(){}=@*$]/gm," ")
+    var tmp = tmp.replace(/[%#;‘’“'"—_—\-,.!:?(){}=@*$]/gm," ")
     var tmp = tmp.replace(/[\/\]\[]/gm," ")
     var rawWords = tmp.split(' ')
     // handles the trimming of white spaces
@@ -97,50 +134,67 @@ function filtering(body,callback){
            && !w.endsWith(' ')){
             filteredWords.push(w);
         }
+        // console.log(filteredWords)
     })
-    callback(filteredWords);
+    var total = filteredWords.length
+    callback(null,filteredWords,total);
 }
 
-function createPostings(filteredWords,callback){
+
+function createPostings(filteredWords,total,callback){
   var postings = []
-  filteredWords.forEach(function(w){
-      if(postings[w]==undefined){
-        postings[w]=1
-      }else{
-        postings[w]= postings[w]+1;
-      }
-  })
 
-  callback(postings)
+  filteredWords.forEach(function(w){
+
+      if(postings.w==undefined){
+        postings.push([w, 1]);
+      }else{
+        postings.w= postings.w+1;
+      }
+
+  })
+  console.log('  - Successful creation of the postings '+Object.keys(postings).length)
+  callback(null,sortTableByValue(postings),total)
 }
+
 
 function calculateFrequencies(postings, totalNbWords, callback){
     var frequencies = []
     postings.forEach(function(p){
-      word = p[0]
+
+      word = p[1][0]
       if(word!=''){
-        value = Math.round(p[1]*100000000/totalNbWords)/100000000
-        frequencies[word] = value
+        value = Math.round(p[1][1]*100000000/totalNbWords)/100000000
+        frequencies.push([word,value])
       }
     })
-    console.log(frequencies)
-    callback(frequencies)
+    console.log('  - Successful creation of the frequency postings '+Object.keys(frequencies).length)
+    callback(null,cleanTable(frequencies))
 }
+
 
 function displayAssociativeTable(table){
+  var limit = 5
+  var count = 0
   for(key in table){
-    console.log(key+' | '+table[key]+'\n');
+    count ++
+    if(count<= limit){
+      console.log(key+' | '+table[key]+'\n');
+    }
   }
 }
 
-// temporary function to display all words parsed after the split and regex
-function displaySimpleTable(table){
-  var monString = "";
-  for(element in table){
-    monString += table[element]+' | '
+function cleanTable(actual) {
+  var newTable = [];
+  for (key in actual) {
+    if (actual[key]!=null) {
+    //  console.log(actual[key])
+      newTable[key] = actual[key];
+    }
   }
-  console.log(monString)
+  return newTable;
 }
+
 
 // sorts the table returning the highest value firsts in descending order
 // example ['key1' -> 2 , 'key2' -> 15 , 'key3' -> 8]
